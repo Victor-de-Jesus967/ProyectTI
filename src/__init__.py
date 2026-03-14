82d04124c92ac093a410ae94dce26a42f29ffdf6
Victor-de-Jesus967 <mijangos.jimenez967@outlook.es>
2026-03-14 11:52:38 -0600

    Update author name in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -4,5 +4,5 @@ Contiene toda la lógica de la aplicación.
 """
 
 __version__ = "1.0.0"
-__author__ = "Tu Nombre"
+__author__ = "Victor de Jesus Jimenez Mijangos"
 __description__ = "Sistema de Gestión de Equipos de Cómputo"
